02b9498370652a01468e46c675e9f90180329072
Abhishek <Abhishekmj04@gmail.com>
2020-07-02 00:47:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('change-color-1').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#EEEDF0'
+})
+
+document.getElementById('change-color-2').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#A1B5C1'
+})
+
+document.getElementById('change-color-3').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#F9ACA7'
+})
+
+document.getElementById('change-color-4').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#68747D'
+})
+
+document.getElementById('change-color-5').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#CF365F'
+})
+
+document.getElementById('change-color-6').addEventListener('click', () => {
+    document.querySelector('.body').style.background = '#EBEBF3'
+})
