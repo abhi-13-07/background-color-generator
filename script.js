f9184443d5903b091791dcee8386d2e65733fb17
Abhishek <Abhishekmj04@gmail.com>
2020-07-02 12:48:46 +0530

    modified script.js & index.html

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-document.getElementById('change-color-1').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#EEEDF0'
-})
+const changeButtonElement = document.getElementsByClassName('btn')
+var body = document.querySelector('.body')
 
-document.getElementById('change-color-2').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#A1B5C1'
-})
+for (var i = 0; i < changeButtonElement.length; i++) {
+    var changeButton = changeButtonElement[i]
+    changeButton.addEventListener('click', (e) => {
+        var button = e.target
+        if (button.id === 'change-color-1') {
+            document.querySelector('.body').style.background = '#EEEDF0'
+        }
 
-document.getElementById('change-color-3').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#F9ACA7'
-})
+        if (button.id === 'change-color-2') {
+            document.querySelector('.body').style.background = '#A1B5C1'
+        }
 
-document.getElementById('change-color-4').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#68747D'
-})
+        if (button.id === 'change-color-3') {
+            document.querySelector('.body').style.background = '#F9ACA7'
+        }
 
-document.getElementById('change-color-5').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#CF365F'
-})
+        if (button.id === 'change-color-4') {
+            document.querySelector('.body').style.background = '#68747D'
+        }
 
-document.getElementById('change-color-6').addEventListener('click', () => {
-    document.querySelector('.body').style.background = '#EBEBF3'
-})
+        if (button.id === 'change-color-5') {
+            document.querySelector('.body').style.background = '#CF365F'
+        }
+
+        if (button.id === 'change-color-6') {
+            document.querySelector('.body').style.background = '#EBEBF3'
+        }
+    })
+}
